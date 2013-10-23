d1fb891497f4ac67c3089bde64609416b71c953c
Rickard Andersson <rickard.m.andersson@gmail.com>
2013-10-23 11:36:17 +0200

    Put MOTD window in own function for easy usage in other program.

--- gui.rkt
+++ gui.rkt
@@ -4,42 +4,48 @@
          "remote-data.rkt" ; remote-location = url-string, obscured for git-repo
          "obj-maker.rkt")
 
-;; Set up a future
-;; This might not be run in parallel because futures apparently
-;; don't handle some operations very well.
-;; So the remote fetch might end up being run straight up instead.
-(define future-objects (future (lambda ()
-                                 (make-objects remote-location))))
+(provide motd)
 
-(define (spawn-objects object-blob)
-  (define (spawn-object obj)
-    (cond
-     [(title? obj) (new message% [parent title-panel] [label (title-text obj)]
-                        [font (title-font obj)])]
-     [(style-data? obj)
-      (send paragraph-editor change-style (style-data-delta obj))]
-     [(paragraph? obj) (add-paragraph (paragraph-text obj))]))
-  (for-each spawn-object object-blob))
+(define (motd window-title window-height window-width)
+  ;; Set up a future
+  ;; This might not be run in parallel because futures apparently
+  ;; don't handle some operations very well.
+  ;; So the remote fetch might end up being run straight up instead.
+  (define future-objects (future (lambda ()
+                                   (make-objects remote-location))))
 
-(define top-frame (new frame% [label "Hey, You. :)"]
-                       [min-height 640] [min-width 480]))
+  (define (spawn-objects object-blob)
+    (define (spawn-object obj)
+      (cond
+       [(title? obj) (new message% [parent title-panel] [label (title-text obj)]
+                          [font (title-font obj)])]
+       [(style-data? obj)
+        (send paragraph-editor change-style (style-data-delta obj))]
+       [(paragraph? obj) (add-paragraph (paragraph-text obj))]))
+    (for-each spawn-object object-blob))
 
-(define title-panel (new horizontal-panel% [parent top-frame]
-                         [vert-margin 8] [alignment '(center top)]
-                         [stretchable-width #f] [stretchable-height #f]))
+  (define top-frame (new frame% [label window-title]
+                         [min-height window-height] [min-width window-width]))
 
-(define paragraph-panel (new horizontal-panel% [parent top-frame]
-                             [alignment '(center top)] [horiz-margin 24]))
+  (define title-panel (new horizontal-panel% [parent top-frame]
+                           [vert-margin 8] [alignment '(center top)]
+                           [stretchable-width #f] [stretchable-height #f]))
 
-(define paragraph-editor (new text% [auto-wrap #t] [line-spacing 5]))
+  (define paragraph-panel (new horizontal-panel% [parent top-frame]
+                               [alignment '(center top)] [horiz-margin 24]))
 
-(define (add-paragraph paragraph-content)
-  (send paragraph-editor insert (string-append paragraph-content "\n\n")))
+  (define paragraph-editor (new text% [auto-wrap #t] [line-spacing 5]))
 
-(define paragraph-canvas (new editor-canvas% [parent paragraph-panel]
-                              [editor paragraph-editor] [enabled #f]
-                              [style '(no-border auto-hscroll auto-vscroll)]))
+  (define (add-paragraph paragraph-content)
+    (send paragraph-editor insert (string-append paragraph-content "\n\n")))
 
-(spawn-objects (touch future-objects))
+  (define paragraph-canvas (new editor-canvas% [parent paragraph-panel]
+                                [editor paragraph-editor] [enabled #f]
+                                [style '(no-border auto-hscroll auto-vscroll)]))
 
-(send top-frame show #t)
+  (spawn-objects (touch future-objects))
+
+  (send top-frame show #t))
+
+(module+ main
+  (motd "Hey, you. :)" 640 480))
