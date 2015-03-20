c8946592314c2828dbd9cd1ff0771d1cdbdd636e
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-03-20 10:42:41 +0200

    Added match call to process URIs to `get-obj-data`

--- dl.rkt
+++ dl.rkt
@@ -1,10 +1,21 @@
 #lang racket
 
-(require net/url)
+(require net/url
+         "datatypes.rkt")
+
 (provide get-obj-data)
 
-(define (get-obj-data url)
-  (call/input-url (string->url url)
-                  get-pure-port
-                  read
-                  '("Cache-Control: no-cache")))
+(define (get-obj-data uri)
+  (define (get-remote-data source)
+    (call/input-url (string->url source)
+                    get-pure-port
+                    read
+                    '("Cache-Control: no-cache")))
+
+  (define (get-local-data source)
+    (call-with-input-file source read))
+
+  (match uri
+    [(pregexp #px"^http://(.*)" (list fm sm)) (get-remote-data sm)]
+    [(pregexp #px"^file://(.*)" (list fm sm)) (get-local-data sm)]
+    [_ (paragraph "Unable to access obj file; invalid protocol.")]))
